67aa9686d73e76e5587b87477f956ffca882af07
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-09-05 21:15:54 -0300

    feat: migrations added

---
+++ teste-php-junior/database/migrations/2023_09_05_235303_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name');
+            $table->decimal('price');
+            $table->bigInteger('bar_code')->unique();
+            $table->integer('stock_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
